ddd27eea4fc893858ea563475de56ced9aad6cb4
Aryak-Kumar <63341965+Aryak-Kumar@users.noreply.github.com>
2020-05-09 21:06:12 +0530

    Update structure.h

--- structure.h
+++ structure.h
@@ -4,23 +4,23 @@
 #include<string>
 using namespace std;
 
-struct card{ 
-	int suit;
-	int face;  
-	bool face_up;
+struct card{ //structure to store the cards
+	int suit; //keeps track of the suit of the card
+	int face; //keeps track of the value of the card
+	bool face_up; //keeps track of whether the card is face up or face down
 };
 
-struct profile{ 
-	string name; 
-	string User; 
-	int chip;
-	string password;
+struct profile{ //structure to store the profile of each player
+	string name; //name of the user
+	string User; //username of the user
+	int chip; //the amount the user has
+	string password; //the password of the user
 };
 
-struct person{ 
-	int bet;
-	vector<card> cards;
-	profile info;
+struct person{ //structure to store the Information for each player for the game 
+	int bet; //the bet made by the person
+	vector<card> cards; //the cards that the person has
+	profile info; //the profile of the player 
 };
 
 #endif
